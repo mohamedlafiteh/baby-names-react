5bc3263d72682955476da23109cf46d93587c3f4
Hanaif <hanaif1@gmail.com>
2020-06-03 17:31:13 +0100

    create new App

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
-import BabyNames from './BabyNames'
+
 import babyNamesData from './babyNamesData.json';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Search 
-      <BabyNames names={} />
+      <h1>Hello from App </h1>
+      
+
     </div>
   );
 }
